4d3b0d759346203a985cfaa0f042857b2f50f86f
landing-russia <landing-russia@yandex.ru>
2022-09-19 08:28:02 +0300

    nav fix

--- components/ui/NavBar.vue
+++ components/ui/NavBar.vue
@@ -8,6 +8,7 @@
     <NuxtLink
       to="/blog"
       class="bg-slate-100 hover:bg-slate-300 leading-none pt-1 pb-1.5 px-5 rounded-3xl font-semibold text-xl text-slate-800 transition duration-150"
+      :class="/^\/blog/.test($route.path) ? 'isparent' : ''"
       >Blog</NuxtLink
     >
     <NuxtLink
@@ -18,24 +19,14 @@
     <NuxtLink
       to="/nuxt-vue"
       class="bg-slate-100 hover:bg-slate-300 leading-none pt-1 pb-1.5 px-5 rounded-3xl font-semibold text-xl text-slate-800 transition duration-150"
+      :class="/^\/nuxt-vue/.test($route.path) ? 'isparent' : ''"
       >NUXT VUE</NuxtLink
     >
-    <div>{{ /^\/blog/.test($route.path) }}</div>
-    <!-- <div>{{ id }}</div> -->
   </nav>
 </template>
 
 <script setup>
-// import { computed } from "vue"
-
-const route = useRoute()
-// const id = computed(() => route.value.params)
-
-// console.log(id)
-
-// const isparent = computed(() => {
-//   return $route.path === ""
-// })
+  
 </script>
 
 <style scoped>
